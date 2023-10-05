42189c44a424a3b8567d2b4d36511b13cb2e9562
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-10-05 07:08:08 -0700

    task

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-"""  script (based on the file 1-pack_web_static.py) 
-     that distributes an archive to your web servers,
-"""
+"""  script (based on the file 1-pack_web_static.py"""
 
-from fabric.api import * 
+
+from fabric.api import *
 import os
 
 env.user = 'ubuntu'
 env.hosts = ['ubuntu@3.85.148.98', '34.234.204.60']
+
 
 @task
 def do_deploy(archive_path):
@@ -17,17 +17,19 @@ def do_deploy(archive_path):
             return False
         file_withex = os.path.basename(archive_path)
         file_noex, ex = os.path.splitext(file_withex)
-        put (archive_path, "/tmp/")
+        put(archive_path, "/tmp/")
         run("rm -f {}".format(archive_path))
         run("mkdir -p /data/web_static/releases/{}/".format(file_noex))
-        run ("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file_withex, file_noex))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(file_withex, file_noex))
         run("rm /tmp/{}".format(file_withex))
-        run("mv /data/web_static/releases/{0}/web_static/* /data/web_static/releases/{0}/".format(file_noex))
+        run("mv /data/web_static/releases/{0}/web_static/* "
+            "/data/web_static/releases/{0}/".format(file_noex))
         run("rm -rf /data/web_static/releases/{}/web_static".format(file_noex))
         run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(file_noex))
+        run("ln -s /data/web_static/releases/{}/ "
+            "/data/web_static/current".format(file_noex))
         print("New version deployed!")
         return True
     except Exception:
         return False
-
