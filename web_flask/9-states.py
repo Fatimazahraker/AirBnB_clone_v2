173dba3910c8d92729876ca36b28ded2234824cf
Fatimazahraker <fatimazahra.kerdoun@gmail.com>
2023-10-22 14:14:56 -0700

    task

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -79,5 +79,22 @@ def close(error):
     storage.close()
 
 
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def states(id=None):
+    """ Route function for /states and /states/<id> """
+    not_found = False
+    if id is not None:
+        states = storage.all(State, id)
+        with_id = True
+        if len(states) == 0:
+            not_found = True
+    else:
+        states = storage.all(State)
+        with_id = False
+    return render_template('9-states.html', states=states,
+                           with_id=with_id, not_found=not_found)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
